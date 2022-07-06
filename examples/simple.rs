67a6d9daca76f12f4ce89d4dbde7b973c4570a6c
yangfengzzz <yangfengzzz@hotmail.com>
2022-07-06 20:38:58 +0800

    first commit

---
+++ examples/simple.rs
@@ -0,0 +1,10 @@
+use bevy::prelude::*;
+use arche_rs::*;
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(Color::BLACK))
+        .insert_resource(Msaa { samples: 4 })
+        .add_plugins(DefaultPlugins)
+        .run();
+}
