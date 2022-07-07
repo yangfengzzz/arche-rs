a78e1a941c204ade55eb6ca99f2df94e99742a7b
yangfengzzz <yangfengzzz@hotmail.com>
2022-07-07 09:25:20 +0800

    add new example

---
+++ examples/marble_pour.rs
@@ -0,0 +1,86 @@
+use bevy::{core::FixedTimestep, prelude::*};
+use arche_rs::*;
+use rand::random;
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.8, 0.8, 0.9)))
+        .insert_resource(Msaa { samples: 4 })
+        .add_plugins(DefaultPlugins)
+        .add_plugin(XPBDPlugin::default())
+        .add_startup_system(startup)
+        .add_system_set(
+            SystemSet::new()
+                .with_run_criteria(FixedTimestep::step(1. / 20.))
+                .with_system(spawn_marbles),
+        )
+        .add_system(despawn_marbles)
+        .run();
+}
+
+struct Materials {
+    blue: Handle<StandardMaterial>,
+}
+
+struct Meshes {
+    sphere: Handle<Mesh>,
+}
+
+fn startup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.insert_resource(Meshes {
+        sphere: meshes.add(Mesh::from(shape::Icosphere {
+            radius: 1.,
+            subdivisions: 4,
+        })),
+    });
+
+    commands.insert_resource(Materials {
+        blue: materials.add(StandardMaterial {
+            base_color: Color::rgb(0.4, 0.4, 0.6),
+            unlit: true,
+            ..Default::default()
+        }),
+    });
+
+    commands.spawn_bundle(OrthographicCameraBundle {
+        transform: Transform::from_translation(Vec3::new(0., 0., 100.)),
+        orthographic_projection: OrthographicProjection {
+            scale: 0.01,
+            ..Default::default()
+        },
+        ..OrthographicCameraBundle::new_3d()
+    });
+}
+
+fn spawn_marbles(mut commands: Commands, materials: Res<Materials>, meshes: Res<Meshes>) {
+    let radius = 0.1;
+    let pos = Vec2::new(random::<f32>() - 0.5, random::<f32>() - 0.5) * 0.5 + Vec2::Y * 3.;
+    let vel = Vec2::new(random::<f32>() - 0.5, random::<f32>() - 0.5);
+    commands
+        .spawn_bundle(PbrBundle {
+            mesh: meshes.sphere.clone(),
+            material: materials.blue.clone(),
+            transform: Transform {
+                scale: Vec3::splat(radius),
+                translation: pos.extend(0.),
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .insert_bundle(ParticleBundle {
+            collider: CircleCollider { radius },
+            ..ParticleBundle::new_with_pos_and_vel(pos, vel)
+        });
+}
+
+fn despawn_marbles(mut commands: Commands, query: Query<(Entity, &Pos)>) {
+    for (entity, pos) in query.iter() {
+        if pos.0.y < -20. {
+            commands.entity(entity).despawn();
+        }
+    }
+}
